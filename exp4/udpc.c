900dd1f9756c4a329b5a9e46eb7fa72b034e2a08
Akash Kumar L <96628545+Akashkumarl@users.noreply.github.com>
2023-03-14 15:27:09 +0530

    Create udpc.c

---
+++ exp4/udpc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<netdb.h>
+
+int main(int argc,char* argv[])
+{
+ struct sockaddr_in server,client;
+ if(argc!=3)
+ 	printf("input format not corrected");
+ int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+ if(sockfd==-1)
+	printf("error in socket(),");
+server.sin_family = AF_INET;
+server.sin_addr.s_addr = INADDR_ANY;
+server.sin_port = htons(atoi(argv[2]));
+
+char buffer[100];
+printf("enter a message to be sent to server");
+fgets(buffer,100,stdin);
+if(sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*) & server,sizeof(server))<0)
+	printf("error in sendto");
+  return 0;
+}	
